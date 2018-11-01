906fa7fceff94b33b375e2919fdb3c21aeced68c
Matt Peters <matthew.george.peters@gmail.com>
2018-11-01 16:51:51 -0400

    wokring on map css

--- neighborhood-map/src/comp/map-comp/GMap.js
+++ neighborhood-map/src/comp/map-comp/GMap.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+
 export class GMapContainer extends Component {
 
     render() { // Cited from: https://www.npmjs.com/package/google-maps-react 11.01.18
+        const style = {
+            width: '50%',
+            height: '50%'
+        }
         return (
             <div id="map" className="g-map">
                    <Map
                         google= { this.props.google }
-                        zoom= { 14 } >
+                        zoom= { 14 } 
+                        style={ style }>
                         <Marker
                             onClick= { this.onMarkerClick }
                             name= { 'Current Location'}
