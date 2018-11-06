625f0c7be599a3186bad2e43a96ca914342add6e
Matt Peters <matthew.george.peters@gmail.com>
2018-11-05 19:58:01 -0500

    hard coded foursquare ID's

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -9,37 +9,42 @@ class App extends Component {
       {
         'name': "The City College of New York",
         'type': "School",
-        'lat': 40.820210,
-        'lon': -73.950348,
-        'street': "160 Convent Ave, New York, NY 10031"
+        'lat': 40.820056869904384,
+        'lon': -73.95030711337873,
+        'street': "160 Convent Ave, New York, NY 10031",
+        'fsquareID': "4bcdbfbc8920b7133b2ea0dc"
       },
       {
         'name': "Trinity Church Cemetery and Mausoleum",
         'type': "Cemetary/Mausoleum",
-        'lat': 40.834370,
-        'lon': -73.949120,
-        'street': "770 Riverside Dr, New York, NY 10032"
+        'lat': 40.83241031722021,
+        'lon': -73.94631768901912,
+        'street': "770 Riverside Dr, New York, NY 10032",
+        'fsquareID': "4c8fc549590ab1f754cbe17d"
       },
       {
         'name': "Hamilton's Bakery",
         'type': "Bakery",
         'lat': 40.827370,
         'lon': -73.949250,
-        'street': "3570 Broadway, New York, NY 10031"
+        'street': "3570 Broadway, New York, NY 10031",
+        'fsquareID': "572e30ddcd104595c82ca52e"
       },
       {
         'name': "Morris-Jumel Mansion",
         'type': "Historical Landmark",
         'lat': 40.834450,
         'lon': -73.938560,
-        'street': "65 Jumel Terrace, New York, NY 10032"
+        'street': "65 Jumel Terrace, New York, NY 10032",
+        'fsquareID': "4b69934ef964a520fea72be3"
       },
       {
-        'name': "Hamilton Grange National Memorial",
-        'type': "Historical Landmark",
-        'lat': 40.823130,
-        'lon': -73.950500,
-        'street': "414 W 141st St, New York, NY 10031"
+        'name': "Texas Chicken & Burgers",
+        'type': "Fast Food Restaurant",
+        'lat': 40.82486414631794,
+        'lon': -73.95150679467686,
+        'street': "3486 Broadway, New York, NY 10031",
+        'fsquareID': "55a15550498e4dc36c4845c3"
       }
     ]
   }
