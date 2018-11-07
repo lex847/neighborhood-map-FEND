bec9d0847d60b46dbcd60ad78bea90362da62fbf
Matt Peters <matthew.george.peters@gmail.com>
2018-11-06 19:45:00 -0500

    cleaned up comments

--- neighborhood-map/src/comp/map-comp/GMapContainer.js
+++ neighborhood-map/src/comp/map-comp/GMapContainer.js
@@ -4,7 +4,7 @@ import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 export class GMapContainer extends Component {
 
     render() {
-        const GoogleMapDir = withGoogleMap(props => ( //https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb 11.6.18
+        const GoogleMapDir = withGoogleMap((props) =>  //https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb 11.6.18
             <GoogleMap
                 defaultCenter = { { lat: 40.8250585, lng: -73.9476404 } }
                 defaultZoom = { 14 }
@@ -19,7 +19,7 @@ export class GMapContainer extends Component {
                 )
             })}
             </GoogleMap>
-        ));
+        );
         console.log(this.props)
         return (
             <div class='map-container__content'>
@@ -29,6 +29,7 @@ export class GMapContainer extends Component {
                 />
             </div>
         );
+        
     }
 }
 
