51240056f207c79c49d9fb3da73bfa2694fd4c19
Matt Peters <matthew.george.peters@gmail.com>
2018-11-13 18:59:01 -0500

    followed map docs

--- neighborhood-map/src/comp/map-comp/GMapContainer.js
+++ neighborhood-map/src/comp/map-comp/GMapContainer.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { compose, withProps } from "recompose"; // https://tomchentw.github.io/react-google-maps/#usage--configuration 11.13.18
 import Popup from './Popup';
 
-export class GMapContainer extends Component {
+
+/*export class GMapContainer extends Component {
     state ={
         markers: [],
         center: {}
     }
 
     render() {
-        let places = this.props.places;
         
         console.log("Map: " + places)
 
-        const GoogleMapDir = withScriptjs(withGoogleMap((props) =>  //https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb 11.6.18
+        /*const GoogleMapDir = withScriptjs(withGoogleMap((props) =>  //https://medium.com/@yelstin.fernandes/render-a-map-component-using-react-google-maps-5f7fb3e418bb 11.6.18
             <GoogleMap
                 props={ props }
                 places={ places }
@@ -43,20 +44,52 @@ export class GMapContainer extends Component {
             })}
             </GoogleMap>
         ));
-        
+        */
+       const GoogleMapDir = compose( //https://tomchentw.github.io/react-google-maps/#usage--configuration 11.13.18
+        withProps({
+          googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBl2AofdiKYqxJE6ktBJJSDUTlvHgo9OrQ",
+          loadingElement: <div style={{ height: `100%` }} />,
+          containerElement: <div style={{ height: '100%', width: '100%' }}/>,
+          mapElement: <div style={{ height: '100%' }} />,
+        }),
+        withScriptjs,
+        withGoogleMap
+      )((props) =>
+      <GoogleMap
+        defaultZoom={ 15 }
+        defaultCenter={{ lat: 40.8250585, lng: -73.9476404 }}
+      >
+        {props.isMarkerShown && <Marker position={{ lat: 40.8250585, lng: -73.9476404 }} onClick={props.onMarkerClick} />}
+      </GoogleMap>
+    )    
+    class GMapContainer extends React.PureComponent {
+      state = {
+        isMarkerShown: false,
+      }
+    
+      componentDidMount() {
+        this.delayedShowMarker()
+      }
+    
+      delayedShowMarker = () => {
+        setTimeout(() => {
+          this.setState({ isMarkerShown: true })
+        }, 3000)
+      }
+    
+      handleMarkerClick = () => {
+        this.setState({ isMarkerShown: false })
+        this.delayedShowMarker()
+      }
+    
+      render() {
         return (
-            <div className='map-container__content'>
-                <GoogleMapDir
-                    isMarkerShown
-                    googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBl2AofdiKYqxJE6ktBJJSDUTlvHgo9OrQ'
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement= { <div style={{ height: '100%', width: '100%' }}/> }
-                    mapElement= { <div style={{ height: '100%' }} /> }>
-                    </GoogleMapDir>
-            </div>
-        );
-        
+          <GoogleMapDir
+            isMarkerShown={this.state.isMarkerShown}
+            onMarkerClick={this.handleMarkerClick}
+          />
+        )
+      }
     }
-}
 
 export default GMapContainer
